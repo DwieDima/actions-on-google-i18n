872e1c078323bde2d0d1192e2539baf8b5d3fa30
DwieDima <dimitri93@gmx.net>
2019-07-30 11:09:16 +0200

    Update index.js
    added raw and suggestion support

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ class I18n {
     if (options.directory && !this._fileExists(options.directory)) {
       throw new Error(
         `[actions-on-google-i18n] directory "${
-          options.directory
+        options.directory
         }" does not exist.`
       );
     }
@@ -45,7 +45,7 @@ class I18n {
       this.configure();
     }
 
-    const __i18nFactory = conv => {
+    const __ = conv => {
 
       const locales = this.initLocaleFile(conv);
 
@@ -54,25 +54,12 @@ class I18n {
 
         if (!translation) {
           // wring key provided
-          throw new Error(`Error: "${key}" was not found in locales [${ Object.keys(locales) }}]. This is the locales file: ${locales}`);
+          throw new Error(`Error: "${key}" was not found in locales [${Object.keys(locales)}}]. This is the locales file: ${locales}`);
         }
 
         if (Array.isArray(translation)) {
-          let isStringArray = true;
-          for(let i = 0; i < translation.length; i++){
-            if(typeof translation[i] !== "string") {
-              isStringArray = false;
-              break;
-            }
-          }
-          if(isStringArray) {
-            //if array is stringarray, return it.
-            //usage for suggestions
-            return translation;
-          } else {
-            // if there are many utterances for a given key, pick a random one
-            translation = translation[Math.floor((Math.random()*translation.length))]
-          }
+          // if there are many utterances for a given key, pick a random one
+          translation = translation[Math.floor((Math.random() * translation.length))]
         }
 
         if (translation) {
@@ -85,7 +72,7 @@ class I18n {
           else if (typeof translation === "object") {
 
             // if the utterance value is a {text, speech} object
-            if (translation.text || translation.speech) {
+            if (translation.text && translation.speech) {
               translation = {
                 text: this.applyContext(translation.text, context),
                 speech: this.applyContext(translation.speech, context),
@@ -96,19 +83,84 @@ class I18n {
             }
 
           }
-          
+
         }
 
         return translation;
       };
     };
 
+    const __all = conv => {
+
+      const locales = this.initLocaleFile(conv);
+
+      return (key) => {
+        let translation = locales[key];
+
+        if (!translation) {
+          // wring key provided
+          throw new Error(`Error: "${key}" was not found in locales [${Object.keys(locales)}}]. This is the locales file: ${locales}`);
+        }
+
+        return translation;
+      };
+    };
+
+    const __raw = conv => {
+
+      const locales = this.initLocaleFile(conv);
+
+      return (key, context = {}) => {
+        let translation = locales[key];
+
+        if (!translation) {
+          // wring key provided
+          throw new Error(`Error: "${key}" was not found in locales [${Object.keys(locales)}}]. This is the locales file: ${locales}`);
+        }
+
+        if (Array.isArray(translation)) {
+          // if there are many utterances for a given key, pick a random one
+          translation = translation[Math.floor((Math.random() * translation.length))]
+        }
+
+        if (translation) {
+
+          if (typeof translation === "string") {
+            // if the utterance value is a simple text, 
+            // go ahead and apply the context 
+            translation = this.applyContext(translation, context);
+          }
+          else if (typeof translation === "object") {
+
+            // if the utterance value is a {text, speech} object
+            if (translation.text && translation.speech) {
+              translation = {
+                text: this.applyContext(translation.text, context),
+                speech: this.applyContext(translation.speech, context),
+              }
+            }
+            else {
+              throw new Error("Error: only 'text' and 'speech' values are allowed.");
+            }
+
+          }
+
+        }
+
+        return translation.text;
+      };
+    };
+
     // Register a middleware to set i18n function on each conv
     app.middleware(conv => {
-      conv.__ = conv.i18n = __i18nFactory(conv);
+      conv.__ = conv.i18n = __(conv);
+      conv.__all = conv.i18n = __all(conv);
+      conv.__raw = conv.i18n = __raw(conv);
     });
 
-    app.__ = app.i18n = __i18nFactory();
+    app.__ = app.i18n = __();
+    app.__all = app.i18n = __all();
+    app.__raw = app.i18n = __raw();
   }
 
   initLocaleFile(conv) {
@@ -120,7 +172,7 @@ class I18n {
       } else {
         throw new Error(
           `[actions-on-google-i18n] extension "${
-            this.defaultExtension
+          this.defaultExtension
           }" is not allowed. Only "js" and "json" files are allowed.`
         );
       }
@@ -135,7 +187,7 @@ class I18n {
         `[actions-on-google-i18n] file "${file}" does not exist.`
       );
     }
-    
+
     return this.flattenObject(require(file));
   }
 
@@ -148,26 +200,26 @@ class I18n {
     }
     return translation;
   }
-  
-    flattenObject(ob) {
+
+  flattenObject(ob) {
     var toReturn = {};
 
     for (var i in ob) {
-        if (!ob.hasOwnProperty(i)) continue;
+      if (!ob.hasOwnProperty(i)) continue;
 
-        if ((typeof ob[i]) == 'object' && ob[i] !== null && !Array.isArray(ob[i])) {
-            var flatObject = this.flattenObject(ob[i]);
-            for (var x in flatObject) {
-                if (!flatObject.hasOwnProperty(x)) continue;
+      if ((typeof ob[i]) == 'object' && ob[i] !== null && !Array.isArray(ob[i])) {
+        var flatObject = this.flattenObject(ob[i]);
+        for (var x in flatObject) {
+          if (!flatObject.hasOwnProperty(x)) continue;
 
-                toReturn[i + '.' + x] = flatObject[x];
-            }
-        } else {
-            toReturn[i] = ob[i];
+          toReturn[i + '.' + x] = flatObject[x];
         }
+      } else {
+        toReturn[i] = ob[i];
+      }
     }
     return toReturn;
-}
+  }
 
   getLocale(conv) {
     let locale = conv && conv.user && conv.user.locale;
