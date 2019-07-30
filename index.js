7282dc36feb42003348fb17505c9809379f4f704
DwieDima <dimitri93@gmx.net>
2019-07-30 11:33:46 +0200

    Update index.js
    testing

--- index.js
+++ index.js
@@ -153,14 +153,14 @@ class I18n {
 
     // Register a middleware to set i18n function on each conv
     app.middleware(conv => {
-      conv.__ = __(conv);
-      conv.__all = __all(conv);
-      conv.__raw = __raw(conv);
+      conv.__ = conv.__ = __(conv);
+      conv.__all = conv.__all = __all(conv);
+      conv.__raw = conv.__raw = __raw(conv);
     });
 
-    app.__ = app.i18n = __();
-    app.__all = app.i18n = __all();
-    app.__raw = app.i18n = __raw();
+    app.__ = conv.__ = __(conv);
+    app.__all = conv.__all = __all(conv);
+    app.__raw = conv.__raw = __raw(conv);
   }
 
   initLocaleFile(conv) {
